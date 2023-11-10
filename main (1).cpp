470e5e34f47de10b18ff80127b4ca68fa7929031
luis123-tec <141784964+luis123-tec@users.noreply.github.com>
2023-11-10 15:35:36 -0600

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,7 @@
+// Proyecto de programación orientada a objetos
+// Luis Fernando Nieto Vega
+
+#include <iostream>
+using namespace std;
+
+int main() { cout << "Bienvenido al programa de peliculas\n"; }
