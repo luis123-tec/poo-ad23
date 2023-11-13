e0a281c2eea97aacfa4702ab01da4db2044e0245
luis123-tec <141784964+luis123-tec@users.noreply.github.com>
2023-11-12 19:43:43 -0600

    Delete main (1).cpp

--- main (1).cpp
+++
@@ -1,7 +0,0 @@
-// Proyecto de programación orientada a objetos
-// Luis Fernando Nieto Vega
-
-#include <iostream>
-using namespace std;
-
-int main() { cout << "Bienvenido al programa de peliculas\n"; }
