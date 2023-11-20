f8836d8fc4640b267f295b68aa9911314561f210
luis123-tec <141784964+luis123-tec@users.noreply.github.com>
2023-11-19 23:35:13 -0600

    Update Proyecto_POO.cpp
    Cambios hechos; 
    Añadí un menu interactivo, tiene por defecto 1 pelicula y 1 serie y ademas puse el formato para si se quieren añadir más peliculas, use vector par almacenarlas. Además el menú puede mostrar las existentes y crear peliculas y series nuevas y las almacena.

--- Proyecto_POO (1).cpp
+++ Proyecto_POO (1).cpp
@@ -1,41 +1,99 @@
 // Proyecto de programación orientada a objetos
 // Luis Fernando Nieto Vega
 
-#include <iostream>
+// main.cpp
 #include "Peliculas.h"
 #include "Series.h"
 #include "Reviews.h"
-
+#include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
-
 int main() {
-  cout << "Bienvenido al programa de películas\n";
+  vector<Pelicula> peliculas; // Vector para almacenar películas
+  vector<Serie> series; // Vector para almacenar series
+  // Películas y series predefinidas
+  Pelicula pelicula1("El Padrino", 1972);
+  pelicula1.setReview(Review("Un clásico.", 5));
+  peliculas.push_back(pelicula1);
+  Serie serie1("Breaking Bad", 5);
+  serie1.setReview(Review("Una de las mejores series de todos los tiempos.", 5));
+  series.push_back(serie1);
+  // Añade aquí más películas y series si lo deseas
+  // Pelicula pelicula2("Nombre de la Pelicula", Año);
+  // pelicula2.setReview(Review("Comentario de la Pelicula", Calificación));
+  // peliculas.push_back(pelicula2);
 
-  // Crear una película
-  Pelicula pelicula("El Padrino", 1972);
-  pelicula.mostrarInformacion();
+  // Serie serie2("Nombre de la Serie", Temporadas);
+  // serie2.setReview(Review("Comentario de la Serie", Calificación));
+  // series.push_back(serie2);
+  // De esta forma se pueden poner más peliculas desde antes
 
-  // Modificar y mostrar la información de la película usando setters y getters
-  pelicula.setTitulo("The Godfather");
-  pelicula.setAnio(1972);
-  cout << "Película modificada: " << pelicula.getTitulo() << " (" << pelicula.getAnio() << ")\n";
 
-  // Crear una reseña para la película
-  Review reviewPelicula("Una obra maestra", 5);
-  reviewPelicula.mostrarReview();
-
-  // Crear una serie
-  Serie serie("Breaking Bad", 5);
-  serie.mostrarInformacion();
-
-  // Modificar y mostrar la información de la serie usando setters and getters
-  serie.setTitulo("Breaking Bad");
-  serie.setTemporadas(5);
-  cout << "Serie modificada: " << serie.getTitulo() << " | Temporadas: " << serie.getTemporadas() << "\n";
-  
-  // Agregar y mostrar la reseña de la serie
-  Review reviewSerie("Muy adictiva", 4);
-  reviewSerie.mostrarReview();
-
+  // Menú
+  int opcion = 0;
+  string titulo;
+  int anio;
+  int temporadas;
+  string comentario;
+  int calificacion;
+  do {
+    cout << "\nBienvenido al programa de películas y series" << endl;
+    cout << "Menú:" << endl;
+    cout << "1 - Mostrar todas las películas y series" << endl;
+    cout << "2 - Añadir nueva serie" << endl;
+    cout << "3 - Añadir nueva película" << endl;
+    cout << "4 - Salir" << endl;
+    cout << "Por favor, seleccione una opción: ";
+    cin >> opcion;
+    switch (opcion) {
+      case 1:
+        // Mostrar todas las películas y series
+        for (const auto &pelicula : peliculas) {
+          pelicula.mostrarInformacion();
+        }
+        for (const auto &serie : series) {
+          serie.mostrarInformacion();
+        }
+        break;
+      case 2:
+        // Añadir nueva serie
+        cout << "Ingrese el título de la serie: ";
+        cin.ignore();
+        getline(cin, titulo);
+        cout << "Ingrese el número de temporadas: ";
+        cin >> temporadas;
+        cout << "Ingrese el comentario para la serie: ";
+        cin.ignore();
+        getline(cin, comentario);
+        cout << "Ingrese la calificación de la serie: ";
+        cin >> calificacion;
+        series.push_back(Serie(titulo, temporadas));
+        series.back().setReview(Review(comentario, calificacion));
+        break;
+      case 3:
+        // Añadir nueva película
+        cout << "Ingrese el título de la película: ";
+        cin.ignore();
+        getline(cin, titulo);
+        cout << "Ingrese el año de la película: ";
+        cin >> anio;
+        cout << "Ingrese el comentario para la película: ";
+        cin.ignore();
+        getline(cin, comentario);
+        cout << "Ingrese la calificación de la película: ";
+        cin >> calificacion;
+        peliculas.push_back(Pelicula(titulo, anio));
+        peliculas.back().setReview(Review(comentario, calificacion));
+        break;
+      case 4:
+        // Salir
+        cout << "Gracias por usar el programa. Adiós." << endl;
+        break;
+      default:
+        cout << "Opción no válida, intente de nuevo." << endl;
+        break;
+    }
+  } while (opcion != 4);
   return 0;
 }
