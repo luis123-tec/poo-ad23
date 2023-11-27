e0c004c8ba6d8a859a793372f8bca8e52b7c0aed
luis123-tec <141784964+luis123-tec@users.noreply.github.com>
2023-11-26 20:25:07 -0600

    Update Peliculas.h
    Mismo cambio para herencia

--- Peliculas.h
+++ Peliculas.h
@@ -4,60 +4,45 @@
 
 /* El uso de #ifndef, #define, y #endif en archivos de encabezado (header) es para prevenir la inclusión múltiple del mismo archivo, esto no debería ser un problema ya que los archivos terminan en .h pero nunca se sabe */
 
-#include "Reviews.h"
-#include <iostream>
+#include "Audiovisual.h"
 #include <string>
+#include <iostream>
 
-class Pelicula {
+class Pelicula : public AudioVisual {
 private:
-  std::string titulo;
   int anio;
-  Review review;
 
 public:
-  Pelicula(std::string titulo, int anio);
+  Pelicula(std::string titulo, int anio, const Review& review); // Composición: Pelicula 
+  Pelicula(std::string titulo, int anio); // Constructor para main.cpp
   Pelicula();
-  void setTitulo(std::string nuevoTitulo);
-  std::string getTitulo() const;
   void setAnio(int nuevoAnio);
   int getAnio() const;
-  void setReview(const Review& nuevaReview);
-  Review getReview() const;
-  void mostrarInformacion() const;
+  void mostrarInformacion() const override; // Override indica que este método sobrescribe el método virtual de la clase base
 };
-
 // Implementaciones
-Pelicula::Pelicula(std::string titulo, int anio) : titulo(titulo), anio(anio) {}
+// Definiciones de métodos de la clase Pelicula
+// Constructor de la clase Pelicula que implementa la composición.
+// Aquí también se crea un objeto Review dentro del constructor,
+// lo cual indica que Review es parte esencial de la Pelicula y no puede existir sin ella.
 
-Pelicula::Pelicula() : titulo(""), anio(0) {}
+Pelicula::Pelicula(std::string titulo, int anio, const Review& review)
+  : AudioVisual(titulo, review), anio(anio) {}
 
-void Pelicula::setTitulo(std::string nuevoTitulo) {
-  titulo = nuevoTitulo;
-}
+Pelicula::Pelicula(std::string titulo, int anio)
+: AudioVisual(titulo, Review()), anio(anio) {}
 
-std::string Pelicula::getTitulo() const {
-  return titulo;
-}
+Pelicula::Pelicula() : AudioVisual(), anio(0) {}
 
 void Pelicula::setAnio(int nuevoAnio) {
   anio = nuevoAnio;
 }
-
 int Pelicula::getAnio() const {
   return anio;
 }
-
-void Pelicula::setReview(const Review& nuevaReview) {
-  review = nuevaReview;
-}
-
-Review Pelicula::getReview() const {
-  return review;
-}
-
 void Pelicula::mostrarInformacion() const {
-  std::cout << "Pelicula: " << titulo << " (" << anio << ")" << std::endl;
+  AudioVisual::mostrarInformacion();
+  std::cout << "Pelicula: " << getTitulo() << " (" << anio << ")" << std::endl;
   review.mostrarReview();
 }
-
 #endif // PELICULAS_H
